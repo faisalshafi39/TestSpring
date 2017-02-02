0738250beeb00ade62557e8acf31a1ee71fdc610
Imtiyaz Hussain <Imtiyaz.Hussain@bqe.com>
2017-02-02 17:02:37 +0530

    minor change

--- src/main/java/com/test/configuration/BeanConfigurer.java
+++ src/main/java/com/test/configuration/BeanConfigurer.java
@@ -1,13 +1,28 @@
 package com.test.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 import com.test.BaseClass;
+import com.test.bean.Address;
+import com.test.bean.Employee;
 
 @ComponentScan(basePackageClasses=BaseClass.class)
 @PropertySource("classpath:/bean.properties")
+@EnableAspectJAutoProxy
 public class BeanConfigurer {
-
+	/*@Bean
+	public Employee getEmployee(Address address)
+	{
+		Employee employee = new Employee("Imtiyaz", null);
+		return employee;
+	}
+	
+	@Bean 
+	public Address getAddress()
+	{
+		return new Address();
+	}*/
 }
--- src/main/java/com/test/sAspect/EmployeeAspect.java
+++ src/main/java/com/test/sAspect/EmployeeAspect.java
@@ -1,14 +1,17 @@
 package com.test.sAspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class EmployeeAspect {
 	
-	@Before("execution(* Employee.testMethod(..))")
-	public void logBeforeV1(JoinPoint joinPoint)
+	@After("execution(** com.test.bean.Employee.testMethod(..))")
+	public void logBeforeV1()
 	{
 		System.out.println("\n\n^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 		System.out.println("Log Version 1");
